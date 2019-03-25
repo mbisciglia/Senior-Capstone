1c37c9fc9df8edda51eea4e8558c09ab697298cb
Michael Bisciglia <mbisciglia@carthage.edu>
2019-03-25 16:34:09 -0500

    big fixes to my response, gets, and posts
    very good, thank you Doug

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var firebase = require('firebase')
 
 
 const app = express();
-const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 
 app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');     
+app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -18,32 +18,32 @@ app.use(express.static('public'));
 
 
 var config = {
-    apiKey: "AIzaSyCLg4YBPW6KMlFoF7nFIr1ZtADSPmxMp80",
-    authDomain: "fir-web-43830.firebaseapp.com",
-    databaseURL: "https://fir-web-43830.firebaseio.com",
-    projectId: "fir-web-43830",
-    storageBucket: "fir-web-43830.appspot.com",
-    messagingSenderId: "567827194064"
-  };
+  apiKey: "AIzaSyCLg4YBPW6KMlFoF7nFIr1ZtADSPmxMp80",
+  authDomain: "fir-web-43830.firebaseapp.com",
+  databaseURL: "https://fir-web-43830.firebaseio.com",
+  projectId: "fir-web-43830",
+  storageBucket: "fir-web-43830.appspot.com",
+  messagingSenderId: "567827194064"
+};
 firebase.initializeApp(config);
-console.log(firebase.auth) // Undefined
+
 
 
 app.get('/', async (req, res) => {
-    res.render('login');
+  res.render('login');
 });
 app.get('/custom', async (req, res) => {
   res.render('custom');
 });
 
 app.get('/map1', async (req, res) => {
-    
-    res.render('map1');
+
+  res.render('map1');
 });
 
 app.get('/map2', async (req, res) => {
-    
-    res.render('map2');
+
+  res.render('map2');
 });
 
 
@@ -55,57 +55,61 @@ app.post('/uploadPhoto', async (req, res) => {
   var file = storageRef.child(imageRef);
 
 
-  ref.put(file).then(function(snapshot) {
+  ref.put(file).then(function (snapshot) {
     console.log('Uploaded a blob or file!');
   });
 
-    res.send({status: 'SUCCESS'});  
-  });
+  res.send({ status: 'SUCCESS' });
+});
 
 app.post('/signIN', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.pass;
+  const email = req.body.email;
+  const password = req.body.pass;
 
+  firebase.auth().signInWithEmailAndPassword(email, password).then(success => {
+    console.log(success);
+    res.status(200).send('SUCCESS');
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    res.status(400).send('FAIL');
+  });
 
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-      res.send({status: 'SUCCESS'});  
-    });
+  
+});
 
 app.post('/createAcc', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.pass;
+  const email = req.body.email;
+  const password = req.body.pass;
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
 
-    res.send({status: 'SUCCESS'});  
+  res.send({ status: 'SUCCESS' });
 });
 
 
 app.post('/forgotPass', async (req, res) => {
-    const email = req.body.email;
+  const email = req.body.email;
 
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
-        console.log("password change email sent");
-      }).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
+  firebase.auth().sendPasswordResetEmail(email).then(function () {
+    console.log("password change email sent");
+  }).catch(function (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
 
-    res.send({status: 'SUCCESS'});  
+  res.send({ status: 'SUCCESS' });
 });
 
 
@@ -115,18 +119,18 @@ app.post('/signIN', async (req, res) => {
   const password = req.body.pass;
 
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-    });
-    res.send({status: 'SUCCESS'});  
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
   });
+  res.send({ status: 'SUCCESS' });
+});
 
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     var name = user.displayName;
     var email = user.email;
@@ -141,7 +145,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 
 app.use((req, res) => {
-    res.status(404).send(`<h2>Uh Oh!</h2><p>Sorry ${req.url} cannot be found here</p>`);
+  res.status(404).send(`<h2>Uh Oh!</h2><p>Sorry ${req.url} cannot be found here</p>`);
 });
 
 
